96fb8da4942c9ca760df3d01fb89bdf64961eb7d
Afiz Savage <savageafiz9@gmail.com>
2021-12-01 09:19:48 +0000

    Add page contents

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,14 +1,36 @@
 import '../styles/styles.css';
 
-function component() {
-    const element = document.createElement('div');
- 
-    // Lodash, now imported by this script
-    element.innerHTML = `Hello, webpack`;
- 
-   element.classList.add('dark');
- 
-    return element;
-  }
- 
- window.onload =  document.body.appendChild(component());
+const todos = [
+  { description: 'Build house', completed: false, index: 0 },
+  { description: 'Build car', completed: false, index: 1 },
+  { description: 'Party', completed: false, index: 2 },
+];
+const list = document.querySelector('ul');
+
+const addHeading = () => {
+  const heading = document.createElement('h1');
+  heading.textContent = 'Today,s Todo';
+  return heading;
+};
+
+const addTopElements = () => {
+  const input = document.createElement('input');
+  document.body.insertBefore(addHeading(), list);
+  document.body.insertBefore(input, list);
+};
+
+const createTodoList = () => {
+  let listItem = '';
+  todos.forEach((todo) => {
+    listItem += `<li> <input type="checkbox" id="C" name="C" value="C"/>  
+    <label>${todo.description}</label></li> `;
+    list.innerHTML = listItem;
+  });
+};
+
+const load = () => {
+  addTopElements();
+  createTodoList();
+};
+
+window.onload = load;
